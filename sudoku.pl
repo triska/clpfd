284e523aa498741dd08766e85ffae9032f52fb64
Markus Triska <triska@metalevel.at>
2015-10-25 12:48:25 +0100

    use all_distinct/1 for stronger propagation

--- sudoku.pl
+++ sudoku.pl
@@ -10,14 +10,14 @@
 sudoku(Rows) :-
         length(Rows, 9), maplist(same_length(Rows), Rows),
         append(Rows, Vs), Vs ins 1..9,
-        maplist(all_different, Rows),
-        transpose(Rows, Columns), maplist(all_different, Columns),
+        maplist(all_distinct, Rows),
+        transpose(Rows, Columns), maplist(all_distinct, Columns),
         Rows = [A,B,C,D,E,F,G,H,I],
         blocks(A, B, C), blocks(D, E, F), blocks(G, H, I).
 
 blocks([], [], []).
 blocks([A,B,C|Bs1], [D,E,F|Bs2], [G,H,I|Bs3]) :-
-        all_different([A,B,C,D,E,F,G,H,I]),
+        all_distinct([A,B,C,D,E,F,G,H,I]),
         blocks(Bs1, Bs2, Bs3).
 
 /* - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - -
