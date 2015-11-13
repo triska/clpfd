2dd34c5224d9dd3a2dfe76a21cc10fd1218c1c03
Markus Triska <triska@metalevel.at>
2015-11-13 18:26:58 +0100

    Improve variable naming. Stefan Kral and students of FHWN.

--- sudoku.pl
+++ sudoku.pl
@@ -12,13 +12,13 @@ sudoku(Rows) :-
         append(Rows, Vs), Vs ins 1..9,
         maplist(all_distinct, Rows),
         transpose(Rows, Columns), maplist(all_distinct, Columns),
-        Rows = [A,B,C,D,E,F,G,H,I],
-        blocks(A, B, C), blocks(D, E, F), blocks(G, H, I).
+        Rows = [As,Bs,Cs,Ds,Es,Fs,Gs,Hs,Is],
+        blocks(As, Bs, Cs), blocks(Ds, Es, Fs), blocks(Gs, Hs, Is).
 
 blocks([], [], []).
-blocks([A,B,C|Bs1], [D,E,F|Bs2], [G,H,I|Bs3]) :-
-        all_distinct([A,B,C,D,E,F,G,H,I]),
-        blocks(Bs1, Bs2, Bs3).
+blocks([N1,N2,N3|Ns1], [N4,N5,N6|Ns2], [N7,N8,N9|Ns3]) :-
+        all_distinct([N1,N2,N3,N4,N5,N6,N7,N8,N9]),
+        blocks(Ns1, Ns2, Ns3).
 
 /* - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - -
    Sample problems.
